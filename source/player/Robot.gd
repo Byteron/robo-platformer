9a508ee36b5711f965472f90ce135b4e148eef9b
dalton5000 <marius.stieme@gmail.com>
2019-12-14 08:49:37 +0100

    merge key+mouse controls

--- source/player/Robot.gd
+++ source/player/Robot.gd
@@ -6,6 +6,7 @@ const DEAD_ZONE = 0.1
 export(NodePath) var camera_path = null
 
 var motion := Vector3()
+var mouse_axis = Vector3()
 
 onready var anim_tree := $AnimationTree
 onready var anim_player := $Robot/AnimationPlayer
@@ -14,6 +15,7 @@ onready var fsm := $FSM
 onready var camera = null
 
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	fsm.host = self
 	camera = get_node(camera_path)
 	fsm.change_state("Idle")
@@ -24,6 +26,8 @@ func _process(delta: float) -> void:
 	if carrot != translation and fsm.is_current_state("Walk"):
 		look_at(carrot, Vector3.UP)
 
+	mouse_axis = lerp (mouse_axis, Vector3.ZERO, 0.1)
+	
 func play(anim_name: String) -> void:
 	var idx = 0
 	if anim_name == "walk": idx = 0
@@ -41,16 +45,33 @@ func get_raw_walk_input_direction() -> Vector3:
 	input_direction.z = Input.get_joy_axis(0, JOY_AXIS_1)
 	input_direction.x = input_direction.x if abs(input_direction.x) > DEAD_ZONE else 0
 	input_direction.z = input_direction.z if abs(input_direction.z) > DEAD_ZONE else 0
+		
+	if Input.is_action_pressed("walk_left"): input_direction.x = -1
+	if Input.is_action_pressed("walk_right"): input_direction.x = 1
+	if Input.is_action_pressed("walk_up"): input_direction.z = -1
+	if Input.is_action_pressed("walk_down"): input_direction.z = 1
+	
 	return input_direction
 
 func get_look_input_direction() -> Vector3:
 	var input_direction := Vector3()
-	input_direction.x = Input.get_joy_axis(0, JOY_AXIS_2)
-	input_direction.y = Input.get_joy_axis(0, JOY_AXIS_3)
-	input_direction.x = input_direction.x if abs(input_direction.x) > DEAD_ZONE else 0
-	input_direction.y = input_direction.y if abs(input_direction.y) > DEAD_ZONE else 0
+	
+	var pad_direction = Vector3()
+	var mouse_direction = mouse_axis
+	
+	pad_direction.x = Input.get_joy_axis(0, JOY_AXIS_2)
+	pad_direction.y = Input.get_joy_axis(0, JOY_AXIS_3)
+	pad_direction.x = pad_direction.x if abs(pad_direction.x) > DEAD_ZONE else 0
+	pad_direction.y = pad_direction.y if abs(pad_direction.y) > DEAD_ZONE else 0
+	
+	input_direction = mouse_direction if mouse_direction.length() > pad_direction.length() else pad_direction
+	
 	return input_direction
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_axis.x += event.relative.normalized().x * 0.1
+		
 func change_state(state: String) -> void:
 	fsm.change_state(state)
 
