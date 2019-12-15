e2df2dc64b3f474349e739af2cee0d611cacca21
dalton5000 <marius.stieme@gmail.com>
2019-12-15 13:11:23 +0100

    fix jump canceling

--- source/player/states/Jump.gd
+++ source/player/states/Jump.gd
@@ -12,12 +12,18 @@ export var friction := 0.4
 
 export var air_drift := 0.1
 
+export var min_upwards_time = 0.1
+var upwards_timer = 0.0
+var canceled = false
+
 func enter(host: Node) -> void:
 	self.host = host as Robot
 	host.play("jump")
 
 func update(host: Node, delta: float) -> void:
 	host = host as Robot
+#	print(host.motion.y)
+	upwards_timer += delta
 
 	var input_direction = host.get_walk_input_direction()*6
 
@@ -27,10 +33,10 @@ func update(host: Node, delta: float) -> void:
 
 	host.motion.y -= Global.GRAVITY * delta
 
-	if Input.is_action_just_released("jump") and host.motion.y > 0.1:
-		host.motion.y = lerp(host.motion.y, 0, friction)
-
-		if host.motion.y < 0.1:
+	if !Input.is_action_pressed("jump") and upwards_timer > min_upwards_time and !canceled:
+		if host.motion.y > 0.0:
+			canceled = true
+			print("jump canceled")
 			host.motion.y = 0
 
 	host.move_and_slide(host.motion, Vector3.UP)
@@ -39,6 +45,9 @@ func update(host: Node, delta: float) -> void:
 		host.fsm.change_state("Idle")
 
 func jump() -> void:
+#	host.transform.origin.y += 0.1
+	canceled=false
+	upwards_timer = 0.0
 	host.motion.y = jump_force
 	jumped = true
 
