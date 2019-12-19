89e3e8ec5ed2b992789c979a8c9554ff2ad8a86a
Aaron Winter <winter.aaron93@gmail.com>
2019-12-19 20:34:11 +0100

    walk acceleration

--- source/player/states/Walk.gd
+++ source/player/states/Walk.gd
@@ -2,31 +2,25 @@ extends State
 
 const DEAD_ZONE := 0.2
 
+var speed := 0.0
 
-export var walk_speed := 6.0
-export var run_speed := 12.0
-export var friction := 0.4
+export var max_speed := 6.0
+
+export var acceleration = 0.6
+
+export var friction := 0.02
 
 export var turn_threshhold := 0.7
-
-
-
-var max_speed
 
 func enter(host: Node) -> void:
 	host = host as Robot
 	host.play("walk")
-	max_speed = walk_speed
 
 func input(host: Node, event: InputEvent) -> void:
 	host = host as Robot
 
 	if event.is_action_pressed("jump"):
 		host.change_state("Jump")
-	if event.is_action_pressed("ui_cancel"):
-		max_speed = run_speed
-	if event.is_action_released("ui_cancel"):
-		max_speed = walk_speed
 
 func update(host: Node, delta: float) -> void:
 	host = host as Robot
@@ -34,13 +28,10 @@ func update(host: Node, delta: float) -> void:
 	var input_direction = host.get_walk_input_direction()
 
 	if input_direction:
-		host.motion = input_direction * max_speed
-		#makes space for a possible turnaround animation
-		#if (input_direction.dot(host.motion) < turn_threshhold) and (host.motion.dot(Vector3(1,1,1)) > 0.3):
-		#	print("turn")
-		#	host.motion = lerp(host.motion, Vector3(0.1,0,0.1), 0.1)
-		#else:
+		speed = clamp(speed + acceleration, 0, max_speed)
+		host.motion = input_direction * speed
 	else:
+		speed = lerp(speed, 0, friction)
 		host.motion.x = lerp(host.motion.x, 0, friction)
 		host.motion.z = lerp(host.motion.z, 0, friction)
 
