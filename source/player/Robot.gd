f8128749a7cd2234e8a6480b0051995032df7a68
Aaron Winter <winter.aaron93@gmail.com>
2019-12-22 19:34:41 +0100

    unlike

--- source/player/Robot.gd
+++ source/player/Robot.gd
@@ -67,9 +67,9 @@ func play(anim_name: String) -> void:
 	if anim_name == "jump": idx = 1
 	if anim_name == "fall": idx = 2
 	if anim_name == "land": idx = 3
-	if anim_name == "like": idx = 4
+	#if anim_name == "like": idx = 4
 
-	if anim_tree.get("parameters/state/current") >= 3:
+	if anim_tree.get("parameters/state/current") == 3:
 		return
 
 	if anim_name == "walk": idx = 0
